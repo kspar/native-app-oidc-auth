1f44717810d0d77ac3f0a70beaee1c16b7e7e80d
Kaspar Papli <paplka@f-secure.com>
2019-07-10 02:33:36 +0300

    use port from predefined range (because idp doesn't support partial-wildcard cors origins...); close browser when tokens are received

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ import conf
 import socket
 import threading
 import webbrowser
+import subprocess
 
+PORT_RANGE_FIRST = 5100
+PORT_RANGE_LAST = 5109
 
 app = Flask(__name__)
+
+port = None
+browser_process = None
 
 
 @app.route('/keycloak.json')
@@ -15,7 +21,7 @@ def keycloak_conf():
 
 @app.route('/login')
 def login():
-    return render_template("login.html", idp_url=conf.IDP_URL, port=5000)
+    return render_template("login.html", idp_url=conf.IDP_URL, port=port)
 
 
 @app.route('/deliver-tokens', methods=['POST'])
@@ -25,6 +31,12 @@ def deliver_tokens():
         print(body['access_token'])
         print(body['refresh_token'])
 
+        if browser_process is not None:
+            print("Terminating")
+            browser_process.terminate()
+        else:
+            print("Browser process is None ???")
+
         return Response(status=200)
 
     else:
@@ -32,19 +44,36 @@ def deliver_tokens():
 
 
 def get_free_port():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('127.0.0.1', 0))
-    port = sock.getsockname()[1]
-    sock.close()
-    return port
+    for p in range(PORT_RANGE_FIRST, PORT_RANGE_LAST + 1):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sock.bind(('127.0.0.1', p))
+            sock.close()
+            return p
+        except OSError:
+            # Port already in use?
+            pass
 
+    raise OSError("Unable to bind to ports {} - {}".format(PORT_RANGE_FIRST, PORT_RANGE_LAST))
+
+
+def open_browser(url):
+    global browser_process
+    browser_name = webbrowser.get().name
+    browser_process = subprocess.Popen([browser_name, url],
+                                       stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+def start_auth():
+    global port
+    port = get_free_port()
+    url = 'http://127.0.0.1:{}/login'.format(port)
+
+    # Assume the server starts in 1 second
+    threading.Timer(1, lambda: open_browser(url)).start()
+
+    app.run(host='127.0.0.1', port=port)
 
 
 if __name__ == '__main__':
-    port = get_free_port()
-    print(port)
-
-    url = 'http://127.0.0.1:{}/login'.format(port)
-    threading.Timer(2, lambda: webbrowser.open(url)).start()
-
-    app.run(host='127.0.0.1', port=port)
+    start_auth()
